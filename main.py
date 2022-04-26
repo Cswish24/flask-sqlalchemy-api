c3340807077dcc764d7706ca76e5179f99fd85a8
Cameron <85236636+Cswish24@users.noreply.github.com>
2022-04-25 23:15:06 -0500

    use sqlalchemy to recreate RESTful api, add store Resource and model as well.

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 from flask import Flask
-from flask_restful import  Api
+from flask_restful import Api
 from flask_jwt import JWT
-from user import UserRegister
+from resources.user import UserRegister
 from security import authenticate, identity
-from item import Item, ItemList
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+from db import db
 
 app = Flask(__name__)
-app.secret_key = "balls"
+app.secret_key = "key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # turns off flask-sqlalchemy tracker, replaces with vanilla sqlalchemy tracker
 api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 # items = []
 
 jwt = JWT(app, authenticate, identity) # creates /auth endpoint, makes wrapper work
 
-#Item and Item list moved to item py, therefore imports jwt_required request, requestparser, and Resource moved
 
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
+api.add_resource(Store, '/store/<name>')
+api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
+    db.init_app(app)
     app.run(port=6000, debug=True)
